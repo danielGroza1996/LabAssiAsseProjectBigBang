857e020fa900b8879b366e1b63d67ddde4d11260
Daniel Groza <danielgroza1990@gmail.com>
2019-04-17 13:41:11 +0300

    Added addGrade tests

--- src/test/java/LabAssiAsseProjectMV/AppTest.java
+++ src/test/java/LabAssiAsseProjectMV/AppTest.java
@@ -1,5 +1,13 @@
 package LabAssiAsseProjectMV;
 
+import LabAssiAsseProjectMV.Domain.Nota;
+import LabAssiAsseProjectMV.Domain.TemaLab;
+import LabAssiAsseProjectMV.Repository.XMLFileRepository.NotaXMLRepo;
+import LabAssiAsseProjectMV.Repository.XMLFileRepository.TemaLabXMLRepo;
+import LabAssiAsseProjectMV.Service.XMLFileService.NotaXMLService;
+import LabAssiAsseProjectMV.Service.XMLFileService.TemaLabXMLService;
+import LabAssiAsseProjectMV.Validator.NotaValidator;
+import LabAssiAsseProjectMV.Validator.TemaLabValidator;
 import org.junit.Test;
 
 import LabAssiAsseProjectMV.Domain.Student;
@@ -7,6 +15,8 @@ import LabAssiAsseProjectMV.Repository.XMLFileRepository.StudentXMLRepo;
 import LabAssiAsseProjectMV.Service.XMLFileService.StudentXMLService;
 import LabAssiAsseProjectMV.Validator.StudentValidator;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.stream.StreamSupport;
 
 import static org.junit.Assert.*;
@@ -26,125 +36,79 @@ public class AppTest
     }
 
     /**
-     * Creating student entities
+     * Creating Domain Entities
      */
-    private Student validStudent1 = new Student("1", "Student1", 1, "std1@gmail.com", "Jane Doe");
-    private Student validStudent2 = new Student("2A", "Student2", 934, "std2@gmail.com", "John Doe");
-    private Student similarIDStudent = new Student("1", "Student3", 20, "std3@gmail.com", "Jim");
-    private Student invalidStudent1 = new Student("", "", 0, "", "Jane Doe");
-    private Student invalidStudent2 = new Student("", "Nume", -1, "Email", "Jane Doe");
+    String str = "2018-10-01 12:30";
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    LocalDateTime dateTime = LocalDateTime.parse(str, formatter);
 
-    private String[] validStudentData1 = {validStudent1.getId(), validStudent1.getNume(), String.valueOf(validStudent1.getGrupa()), validStudent1.getEmail(), validStudent1.getIndrumator()};
-    private String[] validStudentData2 = {validStudent2.getId(), validStudent2.getNume(), String.valueOf(validStudent2.getGrupa()), validStudent2.getEmail(), validStudent2.getIndrumator()};
-    private String[] similarIDStudentData = {similarIDStudent.getId(), similarIDStudent.getNume(), String.valueOf(similarIDStudent.getGrupa()), similarIDStudent.getEmail(), similarIDStudent.getIndrumator()};
-    private String[] invalidStudentData1 = {invalidStudent1.getId(), invalidStudent1.getNume(), String.valueOf(invalidStudent1.getGrupa()), invalidStudent1.getEmail(), invalidStudent1.getIndrumator()};
-    private String[] invalidStudentData2 = {invalidStudent2.getId(), invalidStudent2.getNume(), String.valueOf(invalidStudent2.getGrupa()), invalidStudent2.getEmail(), invalidStudent2.getIndrumator()};
+    private Student student = new Student("1", "Student", 934, "student@gmail.com", "Name");
+    private TemaLab temaLab = new TemaLab(1, "Description", 2, 2);
+    private Nota nota = new Nota(1,"1",1,10.0, dateTime);
 
+    private String[] studentS = {student.getId(), student.getNume(), String.valueOf(student.getGrupa()), student.getEmail(), student.getIndrumator()};
+    private String[] temaLabS = {String.valueOf(temaLab.getId()), temaLab.getDescriere(), String.valueOf(temaLab.getTermenLimita()), String.valueOf(temaLab.getSaptammanaPredarii())};
+    private String[] notaS = {String.valueOf(nota.getId()), nota.getStudentId(), String.valueOf(nota.getTemaLabId()), String.valueOf(nota.getValoare()), "" + nota.getLdt()};
     /**
      * Creating necessary drivers
      */
-    private StudentValidator val = new StudentValidator();
-    private StudentXMLRepo repo;
-    private StudentXMLService serv;
+    private StudentValidator stdVal = new StudentValidator();
+    private StudentXMLRepo stdRepo = new StudentXMLRepo(stdVal, "StudentiXML.xml");
+    private StudentXMLService stdServ = new StudentXMLService(stdRepo);
+
+    private TemaLabValidator tlbVal = new TemaLabValidator();
+    private TemaLabXMLRepo tlbRepo = new TemaLabXMLRepo(tlbVal, "TemaLaboratorXML.xml");
+    private TemaLabXMLService tlbServ = new TemaLabXMLService(tlbRepo);
+
+    private NotaValidator ntVal = new NotaValidator();
+    private NotaXMLRepo ntRepo = new NotaXMLRepo(ntVal, "NotaXML.xml");
+    private NotaXMLService ntServ = new NotaXMLService(ntRepo);
 
     @Test
-    public void tc_2_numberOfStudents() {
+    public void tc_1_addedStudents() {
         try {
-            repo = new StudentXMLRepo(val, "StudentiXML.xml");
-            serv = new StudentXMLService(repo);
-
-            serv.add(validStudentData1);
-            serv.add(validStudentData2);
+            stdServ.add(studentS);
         }
         catch (Exception e) {
             fail();
         }
-        assertEquals(2, serv.getSize());
+        assertEquals(1, stdServ.getSize());
     }
 
     @Test
-    public void tc_0_numberOfStudents() {
+    public void tc_1_addedAssignments() {
         try {
-            repo = new StudentXMLRepo(val, "StudentiXML.xml");
-            serv = new StudentXMLService(repo);
-
-            serv.add(invalidStudentData1);
-            serv.add(invalidStudentData2);
-        }
-        catch (Exception e) {
-            assertEquals(0, serv.getSize());
-        }
-        finally {
-            assertEquals(0, serv.getSize());
-        }
-    }
-
-    @Test
-    public void tc_studentDataCorrect() {
-        try {
-            repo = new StudentXMLRepo(val, "StudentiXML.xml");
-            serv = new StudentXMLService(repo);
-
-            serv.add(validStudentData1);
-            serv.add(validStudentData2);
+            tlbServ.add(temaLabS);
         }
         catch (Exception e) {
             fail();
         }
-        Iterable<Student> students = serv.findAll();
-        Student[] stds = StreamSupport.stream(students.spliterator(), false).toArray(Student[]::new);
-
-        assertEquals("1", stds[0].getId());
-        assertEquals("Student1", stds[0].getNume());
-        assertEquals(1, stds[0].getGrupa());
-        assertEquals("std1@gmail.com", stds[0].getEmail());
-        assertEquals("Jane Doe", stds[0].getIndrumator());
-
-        assertEquals("2A", stds[1].getId());
-        assertEquals("Student2", stds[1].getNume());
-        assertEquals(934, stds[1].getGrupa());
-        assertEquals("std2@gmail.com", stds[1].getEmail());
-        assertEquals("John Doe", stds[1].getIndrumator());
+        assertEquals(1, tlbServ.getSize());
     }
 
     @Test
-    public void tc_correctErrors_invalidStudent1() {
+    public void tc_1_addedGrades() {
         try {
-            repo = new StudentXMLRepo(val, "StudentiXML.xml");
-            serv = new StudentXMLService(repo);
-
-            serv.add(invalidStudentData1);
-        }
-        catch (Exception e) {
-            assertEquals("Id invalid\nNume invalid\nGrupa invalid\nEmail invalid\n", e.getMessage());
-        }
-    }
-
-    @Test
-    public void tc_correctErrors_invalidStudent2() {
-        try {
-            repo = new StudentXMLRepo(val, "StudentiXML.xml");
-            serv = new StudentXMLService(repo);
-
-            serv.add(invalidStudentData2);
-        }
-        catch (Exception e) {
-            assertEquals("Id invalid\nGrupa invalid\n", e.getMessage());
-        }
-    }
-
-    @Test
-    public void tc_1_numberOfStudents() {
-        try {
-            repo = new StudentXMLRepo(val, "StudentiXML.xml");
-            serv = new StudentXMLService(repo);
-
-            serv.add(validStudentData1);
-            serv.add(similarIDStudentData);
+            ntServ.add(notaS);
         }
         catch (Exception e) {
             fail();
         }
-        assertEquals(1, serv.getSize());
+        assertEquals(1, ntServ.getSize());
+    }
+
+    @Test
+    public void tc_integ_all() {
+        try {
+            stdServ.add(studentS);
+            tlbServ.add(temaLabS);
+            ntServ.add(notaS);
+        }
+        catch (Exception e) {
+            fail();
+        }
+        assertEquals(1, stdServ.getSize());
+        assertEquals(1, tlbServ.getSize());
+        assertEquals(1, ntServ.getSize());
     }
 }
