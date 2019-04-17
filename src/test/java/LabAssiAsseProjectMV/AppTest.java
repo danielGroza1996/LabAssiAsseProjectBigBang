8c9808c006a9bf94b9f13c5325b4da49ea8faa34
Daniel Groza <danielgroza1990@gmail.com>
2019-04-17 11:15:44 +0300

    Added one more test case

--- src/test/java/LabAssiAsseProjectMV/AppTest.java
+++ src/test/java/LabAssiAsseProjectMV/AppTest.java
@@ -1,8 +1,6 @@
 package LabAssiAsseProjectMV;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import LabAssiAsseProjectMV.Domain.Student;
 import LabAssiAsseProjectMV.Repository.XMLFileRepository.StudentXMLRepo;
@@ -10,6 +8,8 @@ import LabAssiAsseProjectMV.Service.XMLFileService.StudentXMLService;
 import LabAssiAsseProjectMV.Validator.StudentValidator;
 
 import java.util.stream.StreamSupport;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
@@ -28,13 +28,15 @@ public class AppTest
     /**
      * Creating student entities
      */
-    private Student validStudent1 = new Student("1", "Student1", 934, "std1@gmail.com", "Jane Doe");
-    private Student validStudent2 = new Student("2A", "Student2", 937, "std2@gmail.com", "John Doe");
+    private Student validStudent1 = new Student("1", "Student1", 1, "std1@gmail.com", "Jane Doe");
+    private Student validStudent2 = new Student("2A", "Student2", 934, "std2@gmail.com", "John Doe");
+    private Student similarIDStudent = new Student("1", "Student3", 20, "std3@gmail.com", "Jim");
     private Student invalidStudent1 = new Student("", "", 0, "", "Jane Doe");
     private Student invalidStudent2 = new Student("", "Nume", -1, "Email", "Jane Doe");
 
     private String[] validStudentData1 = {validStudent1.getId(), validStudent1.getNume(), String.valueOf(validStudent1.getGrupa()), validStudent1.getEmail(), validStudent1.getIndrumator()};
     private String[] validStudentData2 = {validStudent2.getId(), validStudent2.getNume(), String.valueOf(validStudent2.getGrupa()), validStudent2.getEmail(), validStudent2.getIndrumator()};
+    private String[] similarIDStudentData = {similarIDStudent.getId(), similarIDStudent.getNume(), String.valueOf(similarIDStudent.getGrupa()), similarIDStudent.getEmail(), similarIDStudent.getIndrumator()};
     private String[] invalidStudentData1 = {invalidStudent1.getId(), invalidStudent1.getNume(), String.valueOf(invalidStudent1.getGrupa()), invalidStudent1.getEmail(), invalidStudent1.getIndrumator()};
     private String[] invalidStudentData2 = {invalidStudent2.getId(), invalidStudent2.getNume(), String.valueOf(invalidStudent2.getGrupa()), invalidStudent2.getEmail(), invalidStudent2.getIndrumator()};
 
@@ -55,7 +57,7 @@ public class AppTest
             serv.add(validStudentData2);
         }
         catch (Exception e) {
-            assertTrue(false);
+            fail();
         }
         assertEquals(2, serv.getSize());
     }
@@ -87,20 +89,20 @@ public class AppTest
             serv.add(validStudentData2);
         }
         catch (Exception e) {
-            assertTrue(false);
+            fail();
         }
         Iterable<Student> students = serv.findAll();
         Student[] stds = StreamSupport.stream(students.spliterator(), false).toArray(Student[]::new);
 
         assertEquals("1", stds[0].getId());
         assertEquals("Student1", stds[0].getNume());
-        assertEquals(934, stds[0].getGrupa());
+        assertEquals(1, stds[0].getGrupa());
         assertEquals("std1@gmail.com", stds[0].getEmail());
         assertEquals("Jane Doe", stds[0].getIndrumator());
 
         assertEquals("2A", stds[1].getId());
         assertEquals("Student2", stds[1].getNume());
-        assertEquals(937, stds[1].getGrupa());
+        assertEquals(934, stds[1].getGrupa());
         assertEquals("std2@gmail.com", stds[1].getEmail());
         assertEquals("John Doe", stds[1].getIndrumator());
     }
@@ -129,5 +131,20 @@ public class AppTest
         catch (Exception e) {
             assertEquals("Id invalid\nGrupa invalid\n", e.getMessage());
         }
+    }
+
+    @Test
+    public void tc_1_numberOfStudents() {
+        try {
+            repo = new StudentXMLRepo(val, "StudentiXML.xml");
+            serv = new StudentXMLService(repo);
+
+            serv.add(validStudentData1);
+            serv.add(similarIDStudentData);
+        }
+        catch (Exception e) {
+            fail();
+        }
+        assertEquals(1, serv.getSize());
     }
 }
